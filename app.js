5db546a7ed150d40a68c736b54dbe5852199de78
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2026-06-27 00:04:14 +0200

    Update app.js with necessary middleware, EJS view engine and static assets

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ try {
   process.loadEnvFile("./config/.env");
 } catch (error) {}
 
-const { express } = require("express");
+const express = require("express");
 const app = express();
+const path = require("node:path");
+const gamesRouter = require("./routes/gamesRouter");
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", gamesRouter);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running at htpp://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
